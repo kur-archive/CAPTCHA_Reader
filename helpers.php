5c22946ea7b52e7c41a4a13726ce4e682f14d3fd
Amatist_Kurisu <Kuri-su@users.noreply.github.com>
2018-06-12 17:26:01 +0800

    Create helpers.php

---
+++ helpers.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kurisu
+ * Date: 2018/05/02
+ * Time: 16:00
+ */
+
+/**
+ * @param array ...$vars
+ */
+//if(!function_exists('dd')){
+//    function dd(...$vars)
+//    {
+//        foreach ($vars as $var) {
+//            dump($var);
+//        }
+//        exit();
+//    }
+//}
